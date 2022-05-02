a6b6d4e85624ba54131189d8da9111683fda3a72
dbswlsrjs <wlsrjsdlaekd@naver.com>
2022-05-02 10:21:06 +0900

    as

--- AH.cpp
+++ AH.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 int main()
 {
-	cout << "HelloWorld" << endl;
+	int MyMoney = 100;
+	int Operand = 10;
+	
+	cout << MyMoney + Operand << endl;
+	cout << MyMoney - Operand << endl;
+	cout << MyMoney * Operand << endl;
+	cout << MyMoney / Operand << endl;
+	cout << MyMoney % Operand << endl;
+
+	cout << 10 + 2 * 20 << endl;
+
+
 
 	return 0;
 
